86f9e1a20871bd22cb373a8061e9608f76299a0f
Piotr Majkrzak <piotr@majkrzak.dev>
2022-06-22 17:40:37 +0200

    Add record parser

--- src/Data/Adif/Parse.hs
+++ src/Data/Adif/Parse.hs
@@ -4,25 +4,47 @@ import Prelude()
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import Data.String (String)
-import Control.Monad (return, (>>))
+import Control.Monad (return, (>>), void)
 import Data.Function (($))
 import Text.Read (read)
+import Text.Show (Show)
+import Data.Char (toLower)
+import Control.Applicative ((<$>))
 
 
 -- | Parses ADI Data-Specifiers
 -- According to the https://www.adif.org/313/ADIF_313.htm#ADI_Data_Specifiers
 -- Data type indicator T and data length are omitted.
-parseField :: Parser Field
-parseField = do
-  _ <- char '<'
+field :: Parser Field
+field = try $ do
+  void $ char '<'
   name <- many1 $ letter <|> char '_'
-  _ <- char ':'
+  void $ char ':'
   length <- many1 digit
-  _ <- optional $ char ':' >> many letter
-  _ <- char '>'
+  void $ optional $ char ':' >> many letter
+  void $ char '>'
   value <- count (read length) anyChar
-  return $ Field name value
+  spaces
+  return $ Field (toLower <$> name, value)
 
-data Field = Field FieldName FieldData
+-- | Parses ADI Records
+-- According to the https://www.adif.org/313/ADIF_313.htm#ADI_Record
+record :: Parser Record
+record = try $ do
+  fields <- many field
+  eor
+  return $ Record fields
+
+-- | Parses End-Of-Header tag
+eoh :: Parser ()
+eoh = try $ char '<' >> oneOf "Ee" >> oneOf "Oo" >> oneOf "Hh" >> char '>' >> spaces
+
+-- | Parses End-Of-Record tag
+eor :: Parser ()
+eor = try $ char '<' >> oneOf "Ee" >> oneOf "Oo" >> oneOf "Rr" >> char '>' >> spaces
+
+
+newtype Record = Record [Field] deriving Show
+newtype Field = Field (FieldName, FieldData) deriving Show
 type FieldName = String
 type FieldData = String
