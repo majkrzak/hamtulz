f3e71c2ac6ec81f1a5a1cc1e9bddba39724b53c0
Piotr Majkrzak <piotr@majkrzak.dev>
2022-06-28 19:25:53 +0200

    Add emptyRecord helper

--- src/Data/Adif.hs
+++ src/Data/Adif.hs
@@ -6,9 +6,9 @@ module Data.Adif where
 import GHC.Generics (Generic)
 import Data.Char (toLower)
 import Data.Adif.Definition (qsoFields)
-import Language.Haskell.TH (Dec(DataD), Con(RecC), DerivClause(DerivClause), mkName, Bang(Bang), SourceUnpackedness(NoSourceUnpackedness),SourceStrictness(NoSourceStrictness), Type(ConT) )
+import Language.Haskell.TH (Dec(DataD,FunD,SigD),Lit(StringL),Clause(Clause),Body(NormalB),Exp(LitE,AppE,ConE) ,Con(RecC), DerivClause(DerivClause), mkName, Bang(Bang), SourceUnpackedness(NoSourceUnpackedness),SourceStrictness(NoSourceStrictness), Type(ConT) )
 
-$(return [
+$(pure [
   DataD
     []
     (mkName "Record")
@@ -24,8 +24,20 @@ $(return [
     ]
     [ DerivClause Nothing
       [
-        ConT (mkName name) 
+        ConT (mkName name)
         | name <- ["Eq", "Show", "Read", "Generic"]
       ]
-    ] 
+    ]
+  ,SigD (mkName "emptyRecord") (ConT $ mkName "Record")
+  ,FunD
+    (mkName "emptyRecord")
+    [
+      Clause
+        []
+        (
+          NormalB $
+            foldl (\x _ -> AppE x (LitE $ StringL "")) (ConE $ mkName "Record") qsoFields
+        )
+        []
+    ]
  ])
