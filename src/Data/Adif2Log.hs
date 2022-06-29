c92c0c038080e502a3e694c12afe8ee94bd954dd
Piotr Majkrzak <piotr@majkrzak.dev>
2022-06-29 17:56:29 +0200

    Rewrite converter to use typed adif

--- src/Data/Adif2Log.hs
+++ src/Data/Adif2Log.hs
@@ -1,25 +1,26 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Data.Adif2Log (adif2log) where
 
 import qualified Data.Log as Log
-import qualified Data.AdifOld as Adif
-import Data.List (find)
+import qualified Data.Adif as Adif
 import Data.Time (UTCTime, parseTimeOrError, defaultTimeLocale)
 import Data.Maybe (fromJust)
 
 
-adif2log :: Adif.File -> [Log.Record]
-adif2log (Adif.File (_, records)) = map record2record records
+adif2log :: [Adif.Record] -> [Log.Record]
+adif2log = map record2record
   where
     record2record :: Adif.Record -> Log.Record
-    record2record (Adif.Record fields) = Log.Record
-      { Log.datetime = parseTimeOrError True defaultTimeLocale "%Y%m%d%H%M%S" (fromJust (findField "qso_date") <> fromJust (findField "time_on")) :: UTCTime
+    record2record Adif.Record {..} = Log.Record
+      { Log.datetime = parseTimeOrError True defaultTimeLocale "%Y%m%d%H%M%S" (_qso_date <> _time_on) :: UTCTime
       , Log.stations = Just Log.Stations
         { Log.logging = Nothing
         , Log.contacted = Just Log.Station
-          { Log.callsign = findField "call"
+          { Log.callsign = Just _call
           , Log.operator = Nothing
           , Log.location = Just Log.Location
-            { Log.gridsquare = findField "gridsquare"
+            { Log.gridsquare = Just _gridsquare
             , Log.description = Nothing
             }
           , Log.antenna = Nothing
@@ -27,17 +28,12 @@ adif2log (Adif.File (_, records)) = map record2record records
           }
         }
       , Log.connection = Just Log.Connection
-        { Log.band = read <$> findField "band"
-        , Log.mode = read <$> findField "mode"
-        , Log.frequency = read <$> findField "freq"
+        { Log.band = Just $ read _band
+        , Log.mode = Just $ read _mode
+        , Log.frequency = Just $ read _freq
         }
       , Log.report = Just Log.Report
-        { Log.sent = findField "rst_sent"
-        , Log.rcvd = findField "rst_rcvd"
+        { Log.sent = Just _rst_sent
+        , Log.rcvd = Just _rst_rcvd
         }
       }
-      where
-        findField :: String -> Maybe String
-        findField key = do
-         Adif.Field (_, val) <- find (\(Adif.Field(key',_)) -> key == key') fields
-         return val
