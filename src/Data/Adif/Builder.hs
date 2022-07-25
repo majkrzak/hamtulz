41b95b75cb5294a44699db9f1867972bbcc6cf11
Piotr Majkrzak <piotr@majkrzak.dev>
2022-07-25 12:52:49 +0200

    Add adif builder

---
+++ src/Data/Adif/Builder.hs
@@ -0,0 +1,28 @@
+module Data.Adif.Builder (toAdif) where
+
+import Data.Adif (Record(..))
+import Control.Monad (join)
+import Data.Adif.Definition (qsoFields)
+import Language.Haskell.TH.Syntax (Exp(ListE, TupE, LitE, AppE, VarE), Lit (StringL), mkName)
+import Data.Char (toLower)
+
+
+toAdif :: [Record] -> String
+toAdif records = join (mapM (toAdifRecord . fields) records) <> "<eor>" <> "\n"
+  where
+    fields record =
+      $(pure $
+        ListE
+          [ TupE
+            [ Just $ LitE $ StringL name
+            , Just $ AppE (VarE $ mkName ("_" <> (toLower <$> name))) (VarE 'record)
+            ] |name <- qsoFields
+          ]
+       )
+
+toAdifRecord :: [(String, Maybe String)] -> String
+toAdifRecord fields = join (mapM toAdifField fields) <> "<eof>" <> "\n"
+
+toAdifField :: (String, Maybe String) -> String
+toAdifField (key,Just val) = "<" <> key <> ":" <> show (length val) <> ">" <> val <> " "
+toAdifField (_,Nothing) = mempty
