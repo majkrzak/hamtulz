ec7a7a41aa71c002151d742a74cfde84293a34ca
Piotr Majkrzak <piotr@majkrzak.dev>
2022-09-14 19:59:44 +0200

    Add empty grid square check

--- app/check_log/Main.hs
+++ app/check_log/Main.hs
@@ -4,6 +4,7 @@ import Data.Log as Log
 import Data.Yaml.AesonInstances ()
 import Data.Yaml (decodeFileWithWarnings)
 import System.Environment (getArgs)
+import Data.Time.Format.ISO8601 (iso8601Show)
 
 main :: IO ()
 main = do
@@ -14,9 +15,15 @@ main = do
       Right xs -> return xs
   print warning
   print (checkOrder records)
+  print (checkEmptyGrid records)
   mempty
 
 checkOrder ::  [Log.Record] -> [String]
 checkOrder [] = []
 checkOrder [_] = []
-checkOrder (Log.Record{datetime=d1}:x@Log.Record{datetime=d2}:xs) = (["log from " <> show d2 <> " in wrong order" | d1>d2]) <> checkOrder (x:xs)
+checkOrder (Log.Record{datetime=d1}:x@Log.Record{datetime=d2}:xs) = (["log from " <> iso8601Show d2 <> " in wrong order" | d1>d2]) <> checkOrder (x:xs)
+
+checkEmptyGrid :: [Log.Record] -> [String]
+checkEmptyGrid [] = []
+checkEmptyGrid (Log.Record{datetime=d, connection=Just Log.Connection {mode=Just Log.FT8}, stations=Just Log.Stations{contacted=Just Log.Station{location=Just Log.Location{gridsquare = Just ""}}}}:xs) = ["log from " <> iso8601Show d <> " have empty grid"] <> checkEmptyGrid xs
+checkEmptyGrid (_:xs) = checkEmptyGrid xs
