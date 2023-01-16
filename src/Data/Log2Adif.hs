fd2910617670cf525a60e428875828fbb9224c2b
Piotr Majkrzak <piotr@majkrzak.dev>
2023-01-16 17:20:53 +0100

    Export SOTA references to Adif

--- src/Data/Log2Adif.hs
+++ src/Data/Log2Adif.hs
@@ -13,10 +13,15 @@ log2adif = map record2record
   where
     record2record :: Log.Record -> Adif.Record
     record2record r = Adif.emptyRecord
-      { Adif._call = pure r >>= Log.stations >>= Log.contacted >>= Log.callsign
+      { Adif._station_callsign = pure r >>= Log.stations >>= Log.logging >>= Log.callsign
+      , Adif._call = pure r >>= Log.stations >>= Log.contacted >>= Log.callsign
       , Adif._qso_date = pure $ formatTime defaultTimeLocale "%Y%m%d" $ Log.datetime r
       , Adif._time_on = pure $ formatTime defaultTimeLocale "%H%M%S" $ Log.datetime r
       , Adif._band = show <$> (pure r >>= Log.connection >>= Log.band)
       , Adif._freq = show <$> (pure r >>= Log.connection >>= Log.frequency)
       , Adif._mode = show <$> (pure r >>= Log.connection >>= Log.mode)
+      , Adif._my_sota_ref = pure r >>= Log.stations >>= Log.logging >>= Log.location >>= Log.program >>= Log.sota
+      , Adif._sota_ref = pure r >>= Log.stations >>= Log.contacted >>= Log.location >>= Log.program >>= Log.sota
+      , Adif._my_wwff_ref = pure r >>= Log.stations >>= Log.logging >>= Log.location >>= Log.program >>= Log.wwff
+      , Adif._wwff_ref = pure r >>= Log.stations >>= Log.contacted >>= Log.location >>= Log.program >>= Log.wwff
       }
