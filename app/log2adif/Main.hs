cd6a45446bfb2d7021cfc7503a643f3405f025ce
Piotr Majkrzak <piotr@majkrzak.dev>
2023-02-28 18:49:23 +0100

    Generate log in dedicated adif files

--- app/log2adif/Main.hs
+++ app/log2adif/Main.hs
@@ -1,14 +1,58 @@
 module Main where
 
 import Data.Log as Log
+import Data.Log.Lens as Log'
 import Data.Log2Adif(log2adif)
 import Data.Yaml.AesonInstances ()
 import Data.Yaml (decodeFileThrow)
 import Data.Adif.Builder (toAdif)
 import System.Environment (getArgs)
+import Data.Maybe (isJust, fromMaybe)
+import Control.Lens ((^?), _Just, (^.), (^?!))
+import Data.Time (utctDay)
+import Data.Time.Format.ISO8601 (iso8601Show)
+import Data.List (groupBy)
 
 main :: IO ()
 main = do
   [logFile, adifFile] <- getArgs
   records :: [Log.Record] <- decodeFileThrow logFile
-  writeFile adifFile (toAdif $ log2adif records)
+
+  mapM_ ( \records' -> writeFile (adifFile ++ callsignFileName (head records')) (toAdif $ log2adif records')) (callsignRecords records)
+  mapM_ ( \records' -> writeFile (adifFile ++ sotaFileName (head records')) (toAdif $ log2adif records')) (sotaRecords records)
+
+
+callsignFileName :: Log.Record -> String
+callsignFileName  record
+   = safeStroke ( record ^?! (Log'.stations . _Just . Log'.logging . _Just . Log'.callsign . _Just)) ++ ".adif"
+
+callsignRecords :: [Log.Record] -> [[Log.Record]]
+callsignRecords = groupBy (\r1 r2 -> callsignFileName r1 == callsignFileName r2)
+
+sotaFileName :: Log.Record -> String
+sotaFileName record
+  = concat
+    [ "SOTA"
+    , "/"
+    , iso8601Show (utctDay (record ^. Log'.datetime ))
+    , "_"
+    , safeStroke ( record ^?! (Log'.stations . _Just . Log'.logging . _Just . Log'.callsign . _Just))
+    , "_"
+    , safeStroke ( fromMaybe ""  (record ^? (Log'.stations . _Just . Log'.logging . _Just . Log'.location ._Just . Log'.program ._Just . Log'.sota ._Just))  )
+    , ".adif"
+    ]
+
+sotaFilter :: Log.Record -> Bool
+sotaFilter record
+  = isJust (record ^? (Log'.stations . _Just . Log'.logging . _Just . Log'.location ._Just . Log'.program ._Just . Log'.sota ._Just))
+  || isJust (record ^? (Log'.stations . _Just . Log'.contacted . _Just . Log'.location ._Just . Log'.program ._Just . Log'.sota ._Just))
+
+sotaRecords :: [Log.Record] -> [[Log.Record]]
+sotaRecords = groupBy (\r1 r2 -> sotaFileName r1 == sotaFileName r2) . filter sotaFilter
+
+
+safeStroke :: String -> String
+safeStroke =
+  map $ \case
+    '/' -> '-'
+    c -> c
