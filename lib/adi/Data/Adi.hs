22c32d382118cfad630437b4e87b425ccd32e796
Piotr Majkrzak <piotr@majkrzak.dev>
2023-03-08 17:41:16 +0100

    Remove Adi class

--- lib/adi/Data/Adi.hs
+++ lib/adi/Data/Adi.hs
@@ -1,6 +1,5 @@
-module Data.Adi (Field (Field), Record (Record), Header (Header), Document (Document), Adi, fromAdi, toAdi) where
+module Data.Adi (Field (Field), Record (Record), Header (Header), Document (Document)) where
 
-import Data.Adi.Class (Adi, fromAdi, toAdi)
 import Data.Adi.Model (Document (Document), Field (Field), Header (Header), Record (Record))
 import Data.Adi.Read ()
 import Data.Adi.Show ()
--- lib/adi/Data/Adi/Class.hs
+++
@@ -1,7 +0,0 @@
-module Data.Adi.Class (Adi', Adi, fromAdi, toAdi) where
-
-class Adi' a'
-
-class Adi' a' => Adi a' a where
-  fromAdi :: a' -> a
-  toAdi :: a -> a'
