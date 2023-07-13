8993b39ed0771d168bf7998e6d42735b4130d6cb
Piotr Majkrzak <piotr@majkrzak.dev>
2023-07-13 17:59:59 +0200

    Rework file generation

--- app/log2adif/Main.hs
+++ app/log2adif/Main.hs
@@ -1,87 +1,53 @@
 module Main where
 
 import Control.Lens (coerced, (.~), (?~), (^.), (^?), (^?!), _Just)
-import Control.Lens.Helper ((·), mpu)
+import Control.Lens.Helper (mpu, (°), (·))
 import Data.Adif qualified as Adif
 import Data.Empty (empty)
-import Data.List (groupBy)
+import Data.List (group, groupBy, sort)
 import Data.Log as Log
-import Data.Maybe (fromMaybe, isJust)
+import Data.Maybe (fromJust, fromMaybe, isJust, isNothing)
+import Data.Text (splitOn, unpack)
 import Data.Time (utctDay)
 import Data.Time.Format.ISO8601 (iso8601Show)
-import System.Environment (getArgs)
 import Data.Yaml.Parser (readYamlFile)
+import System.Environment (getArgs)
+
+labels :: Log.Record -> [String]
+labels record =
+  mconcat
+    [ ["all", callsign],
+      ["SOTA" <> "/" <> date <> "_" <> callsign | isJust (record ^. Log.stations ° Log.contacted ° Log.location ° Log.program ° Log.sota) && isNothing my_summit],
+      ["SOTA" <> "/" <> date <> "_" <> callsign <> "_" <> safeStroke summit | Just summit <- [my_summit]],
+      ["POTA" <> "/" <> date <> "_" <> callsign <> "_" <> safeStroke park | park <- my_parks]
+    ]
+  where
+    date = iso8601Show (utctDay (record ^. Log.datetime))
+    callsign = safeStroke $ record ^?! Log.stations . _Just . Log.logging . _Just . coerced . Log.callsign . mpu . _Just
+    my_summit = record ^. Log.stations ° Log.logging ° Log.location ° Log.program ° Log.sota . mpu
+    my_parks = map unpack (maybe [] (splitOn ",") (record ^. Log.stations ° Log.logging ° Log.location ° Log.program ° Log.pota))
 
 main :: IO ()
 main = do
   [logFile, adifFile] <- getArgs
   document :: Log.Document <- readYamlFile logFile
   let records = document ^?! contacts . _Just
+  let all_labels = (map head . group . sort) (mconcat (map labels records))
 
-  mapM_ (\records' -> writeFile (adifFile ++ callsignFileName (head records')) (show $ Adif.toAdi $ makeAdif records')) (callsignRecords records)
-  mapM_ (\records' -> writeFile (adifFile ++ sotaFileName (head records')) (show $ Adif.toAdi $ makeAdif records')) (sotaRecords records)
-  mapM_ (\records' -> writeFile (adifFile ++ potaFileName (head records')) (show $ Adif.toAdi $ makeAdif records')) (potaRecords records)
+  mconcat [writeFile (adifFile <> label <> ".adi") (show $ Adif.toAdi $ makeAdif records label) | label <- all_labels]
 
-makeAdif :: [Log.Record] -> Adif.Document
-makeAdif records =
+makeAdif :: [Log.Record] -> String -> Adif.Document
+makeAdif records label =
   [ Adif.header
-      ?~ ( [ Adif.text .~ "Hamtulz generated log file",
-             Adif._adif_ver ?~ "3.1.4"
+      ?~ ( [ Adif.text .~ label,
+             Adif._adif_ver ?~ "3.1.4",
+             Adif._programid ?~ "Hamtulz"
            ]
              · empty
          ),
-    Adif.records .~ toAdif records
+    Adif.records .~ toAdif (filter (elem label . labels) records)
   ]
     · empty
-
-callsignFileName :: Log.Record -> String
-callsignFileName record =
-  safeStroke (record ^?! (Log.stations . _Just . Log.logging . _Just . coerced . Log.callsign . mpu . _Just)) ++ ".adif"
-
-callsignRecords :: [Log.Record] -> [[Log.Record]]
-callsignRecords = groupBy (\r1 r2 -> callsignFileName r1 == callsignFileName r2)
-
-sotaFileName :: Log.Record -> String
-sotaFileName record =
-  concat
-    [ "SOTA",
-      "/",
-      iso8601Show (utctDay (record ^. Log.datetime)),
-      "_",
-      safeStroke (record ^?! (Log.stations . _Just . Log.logging . _Just . coerced . Log.callsign . mpu . _Just)),
-      "_",
-      safeStroke (fromMaybe "" (record ^? (Log.stations . _Just . Log.logging . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.sota . mpu . _Just))),
-      ".adif"
-    ]
-
-sotaFilter :: Log.Record -> Bool
-sotaFilter record =
-  isJust (record ^? (Log.stations . _Just . Log.logging . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.sota . _Just))
-    || isJust (record ^? (Log.stations . _Just . Log.contacted . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.sota . _Just))
-
-sotaRecords :: [Log.Record] -> [[Log.Record]]
-sotaRecords = groupBy (\r1 r2 -> sotaFileName r1 == sotaFileName r2) . filter sotaFilter
-
-potaFileName :: Log.Record -> String
-potaFileName record =
-  concat
-    [ "POTA",
-      "/",
-      iso8601Show (utctDay (record ^. Log.datetime)),
-      "_",
-      safeStroke (record ^?! (Log.stations . _Just . Log.logging . _Just . coerced . Log.callsign . mpu ._Just)),
-      "_",
-      safeStroke (fromMaybe "" (record ^? (Log.stations . _Just . Log.logging . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.pota . mpu . _Just))),
-      ".adif"
-    ]
-
-potaFilter :: Log.Record -> Bool
-potaFilter record =
-  isJust (record ^? (Log.stations . _Just . Log.logging . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.pota . _Just))
-    || isJust (record ^? (Log.stations . _Just . Log.contacted . _Just . coerced . Log.location . _Just . Log.program . _Just . Log.pota . _Just))
-
-potaRecords :: [Log.Record] -> [[Log.Record]]
-potaRecords = groupBy (\r1 r2 -> potaFileName r1 == potaFileName r2) . filter potaFilter
 
 safeStroke :: String -> String
 safeStroke =
